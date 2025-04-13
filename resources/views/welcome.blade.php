91d0ee0517d473270328b88979369f769e9adfa9
shabuaan <69398840+shabuaan@users.noreply.github.com>
2025-04-13 16:16:51 +0500

    Update welcome.blade.php merged

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,97 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container mt-5">
+        <title>Pool Billiard</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Pool Billliard Association
+                </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/">Ranking</a>
+                    <a href="https://laracasts.com">Table Booking</a>
+                    <a href="https://laravel-news.com">Bookings</a>
+                    <a href="https://blog.laravel.com">Shop</a>
+                </div>
+
+                <div class="container mt-5">
     <h1 class="text-center mb-4">Pool Billiard Association - Table Booking</h1>
     <div class="row">
         @for ($i = 1; $i <= 8; $i++)
@@ -26,4 +116,7 @@
         @endfor
     </div>
 </div>
-@endsection
+            </div>
+        </div>
+    </body>
+</html>
