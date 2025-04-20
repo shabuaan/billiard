b4d1b95a2c26021e2290f1d36c14ad13c1118e89
shabuaan <69398840+shabuaan@users.noreply.github.com>
2025-04-20 08:55:51 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pool Billiard Association - Home</title>
     
-    <!-- Bootstrap CSS CDN -->
+    <!-- Bootstrap CSS CDN --> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
