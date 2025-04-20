796f5ae135a49c555dd489ef1ce4b18992e5fb1f
shabuaan <69398840+shabuaan@users.noreply.github.com>
2025-04-20 08:42:14 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,28 +93,6 @@
 
                 <div class="container mt-5">
     <h1 class="text-center mb-4">Pool Billiard Association - Table Booking</h1>
-    <div class="row">
-        @for ($i = 1; $i <= 8; $i++)
-            <div class="col-md-3 mb-4">
-                <div class="card shadow-sm">
-                    <div class="card-header bg-primary text-white">
-                        <h5 class="card-title mb-0">Table {{ $i }}</h5>
-                    </div>
-                    <div class="card-body">
-                        <p class="card-text">Available Booking Slots:</p>
-                        <ul class="list-group">
-                            @foreach (['10AM', '12PM', '2PM', '4PM', '6PM', '8PM'] as $slot)
-                                <li class="list-group-item d-flex justify-content-between align-items-center">
-                                    {{ $slot }}
-                                    <button class="btn btn-sm btn-outline-success">Book</button>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        @endfor
-    </div>
 </div>
             </div>
         </div>
